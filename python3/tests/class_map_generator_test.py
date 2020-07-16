de9dbd100b1b4a020029599057f9656c464dc101
Russ Adams <russ.adams@gmail.com>
2020-07-15 20:25:44 -0400

    WIP Porting JIG to Python3
    Adding integration test for JIG against tree of class files.

--- python3/tests/class_map_generator_test.py
+++ python3/tests/class_map_generator_test.py
@@ -1,9 +1,10 @@
-import unittest
 import pathlib
 import requests
-import tarfile
 import subprocess
 import sys
+import tarfile
+import unittest
+import zipfile
 from class_map_generator import PortedClassMapGenerator
 
 class PortedClassMapGeneratorTest(unittest.TestCase):
@@ -38,9 +39,9 @@ class PortedClassMapGeneratorTest(unittest.TestCase):
         workspaceDirectory.rmdir()
 
     def test_import_java_sources(self):
-        self.__addGuavaToWorkspace()
+        self.__addGuavaToWorkspaceSource()
 
-        # Generate class map for Guava v20
+        # Generate class map for Guava v20 (from source files)
         importPaths = self.PATH_SEPARATOR.join([
             self.IMPORT_DIRECTORY + "guava-20.0/guava-testlib/src",
             self.IMPORT_DIRECTORY + "guava-20.0/guava/src",
@@ -68,17 +69,11 @@ class PortedClassMapGeneratorTest(unittest.TestCase):
         self.assertTrue(linesInSameOrder)
 
     def test_import_java_classes(self):
-        self.__addGuavaToWorkspace()
-        self.__buildMavenPackage(self.IMPORT_DIRECTORY + "guava-20.0/guava-testlib")
-        self.__buildMavenPackage(self.IMPORT_DIRECTORY + "guava-20.0/guava")
-        self.__buildMavenPackage(self.IMPORT_DIRECTORY + "guava-20.0/guava-gwt")
+        self.__addGuavaToWorkspaceClasses()
 
-
-        # Generate class map for Guava v20
+        # Generate class map for Guava v20 (from class files)
         importPaths = self.PATH_SEPARATOR.join([
-            self.IMPORT_DIRECTORY + "guava-20.0/guava-testlib/target/classes",
-            self.IMPORT_DIRECTORY + "guava-20.0/guava/target/classes",
-            self.IMPORT_DIRECTORY + "guava-20.0/guava-gwt/target/classes"
+            self.IMPORT_DIRECTORY + "classes"
         ])
         class_map_generator = PortedClassMapGenerator(self.PATH_SEPARATOR, importPaths, self.DATA_DIRECTORY,
                 self.JAR_CACHE)
@@ -90,7 +85,7 @@ class PortedClassMapGeneratorTest(unittest.TestCase):
         # Verify the count of importable classes in the library
         javaImpTxtFileContents = javaImpTxtFile.read_text()
         javaImpTxtFileLines = javaImpTxtFileContents.split("\n")
-        self.assertEqual(len(javaImpTxtFileLines), 2503)
+        self.assertEqual(len(javaImpTxtFileLines), 2502)
 
         # Verify that the lines in JavaImp.txt are sorted
         javaImpTxtFileSortedLines = sorted(javaImpTxtFileLines)
@@ -109,13 +104,23 @@ class PortedClassMapGeneratorTest(unittest.TestCase):
             sys.exit()
             
 
-    def __addGuavaToWorkspace(self):
+    def __addGuavaToWorkspaceSource(self):
         # Download and extract source code for Guava v20
         localFilename = self.WORKSPACE_DIRECTORY + 'v20.0.tar.gz'
         remoteFilename = 'https://github.com/google/guava/archive/v20.0.tar.gz' 
         self.__downloadFile(localFilename, remoteFilename)
         javaLibraryTar = tarfile.open(localFilename)
         javaLibraryTar.extractall(self.IMPORT_DIRECTORY)
+
+    def __addGuavaToWorkspaceClasses(self):
+        remoteFolder = 'https://github.com/google/guava/releases/download/v20.0/'
+        for fileName in ['guava-20.0.jar', 'guava-gwt-20.0.jar', 'guava-testlib-20.0.jar']:
+            self.__downloadAndExtractJar(self.WORKSPACE_DIRECTORY + fileName, remoteFolder + fileName)
+
+    def __downloadAndExtractJar(self, localFilename, remoteFilename):
+        self.__downloadFile(localFilename, remoteFilename)
+        with zipfile.ZipFile(localFilename) as jarFile:
+            jarFile.extractall(self.IMPORT_DIRECTORY + "classes")
 
     def __downloadFile(self, localFilename, remoteFilename):
         with requests.get(remoteFilename) as r:
